092feb69e57114733e4c86d81875b6e2a670484e
BharathHU <bharathhubharath@gmail.com>
2026-05-06 15:01:49 +0530

    updated

--- modelproject/myapp/models.py
+++ modelproject/myapp/models.py
@@ -8,3 +8,6 @@ class Student(models.Model):
     email=models.EmailField()
     place=models.CharField(max_length=100)
     dob=models.DateField()
+
+    def __str__(self):
+        return self.name
