abcc5e367672d8fee7df7ff1b3f298c29cc8e5af
BharathHU <bharathhubharath@gmail.com>
2026-05-07 22:13:54 +0530

    updated

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -32,11 +32,12 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.auth',#authentication purpose
+    'django.contrib.contenttypes',#
+    'django.contrib.sessions',#
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',#contains css,images,javascripts
+    'myapp'
 ]
 
 MIDDLEWARE = [
--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import view1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
