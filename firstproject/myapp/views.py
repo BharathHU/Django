c96f530ec8fe0c6545bb760aa6d9279498a46c86
BharathHU <bharathhubharath@gmail.com>
2026-05-07 22:15:09 +0530

    updated

--- firstproject/myapp/views.py
+++ firstproject/myapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def view(request):
-    s="Welcome to Django session!!"
+    s="Welcome to Django sessionn!!"
     return HttpResponse(s)
