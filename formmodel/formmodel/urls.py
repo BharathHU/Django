d20522a36287d97e4e3854988f7868d45997493b
BharathHU <bharathhubharath@gmail.com>
2026-05-26 15:04:12 +0530

    updated

--- formmodel/formmodel/urls.py
+++ formmodel/formmodel/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp.views import formview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',formview)
 
 ]
--- formmodel/myapp/forms.py
+++ formmodel/myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from myapp.models import Student
+
 class StudentForm(forms.ModelForm):
     class Meta:
-        model =Student
-        field ='__all__'
+        model = Student
+        fields = '__all__'
