faebf00b2d1930f0fcfadaa8262f22102dd35c42
BharathHU <bharathhubharath@gmail.com>
2026-05-27 15:05:58 +0530

    updated

--- Fakeproject/populate.py
+++ Fakeproject/populate.py
@@ -2,3 +2,26 @@ import os
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fakeproject.settings')
 django.setup()
+from myapp.models import Employee
+from faker import Faker
+f=Faker('en-IN')
+def populate(n):
+    for i in range(n):
+        fname=f.name()
+        femail=f.email()
+        fplace=f.address()
+        fposition=f.job()
+        fdepartment=f.random_element(elements=('HR', 'Finance', 'Engineering', 'Sales', 'Marketing', 'Legal', 'Admin'))
+        fsalary=f.random_int(min=30000,max=150000)
+        Employee.objects.create(
+            name=fname,
+            email=femail,
+            department=fdepartment,
+            place=fplace,
+            position=fposition,
+            salary=fsalary,
+        )
+
+if __name__ == '__main__':
+    populate(20)
+
